f929bcf3b7ec5b9da578152aa9fe4038b71775a7
pcsishun <pcsishun@gmail.com>
2023-06-12 14:14:06 +0700

    update: btn

--- frontend_dd/src/view/ViewRecordPage.vue
+++ frontend_dd/src/view/ViewRecordPage.vue
@@ -24,11 +24,9 @@
                                 <IconRotate/>
                                 <div class="set-desc">Please rotate your phone in this page. </div>
                             </div>
-                            <div class="set-btn-27">
-                                <button class="button-27" @click="haddleNextPage">OK</button>
-                                <div class="set-btn-back">
-                                    <button class="btn-back" @click="haddleBack">Back</button>
-                                </div>
+                            <div class="set-btn-27 flex justify-around">
+                                <button class="button-50" @click="haddleBack">Back</button>
+                                <button class="button-50" @click="haddleNextPage">OK</button>
                             </div>
                         </div>
                     </div>
@@ -192,6 +190,61 @@ export default {
 
     .set-btn-27{
         display: none;
+    }
+}
+
+
+
+
+
+.button-50 {
+    appearance: button;
+    background-color: #000;
+    background-image: none;
+    border: 1px solid #000;
+    border-radius: 4px;
+    box-shadow: #fff 4px 4px 0 0,#000 4px 4px 0 1px;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-family: ITCAvantGardeStd-Bk,Arial,sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 20px;
+    margin: 0 5px 10px 0;
+    overflow: visible;
+    padding: 12px 40px;
+    text-align: center;
+    text-transform: none;
+    touch-action: manipulation;
+    user-select: none;
+    -webkit-user-select: none;
+    vertical-align: middle;
+    white-space: nowrap;
+  }
+  
+  .button-50:focus {
+    text-decoration: none;
+  }
+  
+  .button-50:hover {
+    text-decoration: none;
+  }
+  
+  .button-50:active {
+    box-shadow: rgba(0, 0, 0, .125) 0 3px 5px inset;
+    outline: 0;
+  }
+  
+  .button-50:not([disabled]):active {
+    box-shadow: #fff 2px 2px 0 0, #000 2px 2px 0 1px;
+    transform: translate(2px, 2px);
+  }
+  
+  @media (min-width: 768px) {
+    .button-50 {
+      padding: 12px 50px;
     }
 }
 
